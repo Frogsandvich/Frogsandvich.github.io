fd19b57b0fa4ffc7fd5618ec2228c62a0e488688
Frogsandvich <troy.rosamond@gmail.com>
2016-05-06 11:26:00 +1200

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main() 
+{
+  // Declaring Variables
+  int a, b;
+  int result;
+  
+  // Process
+  a= ; // First number
+  b= ; // Second number
+  result= a b; // Put in calculation between A and B EG: a+b for + 
+  
+  // Printing Result
+  cout << "The answer is ";
+  cout << result;
+  
+  // Terminate Program
+  return 0;
+}
